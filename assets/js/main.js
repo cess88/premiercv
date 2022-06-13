fd006b2fd846eb538f0afab8256515058eff1dda
cecile cecile <cecilecolo88@gmail.com>
2022-06-13 10:13:14 +0200

    dark mode

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+function dark() {
+    let changecol = document.querySelector("body")
+    changecol.style.color = "yellow"
+    changecol.style.background = "grey"
+
+
+let main = document.querySelector("main")
+main.style.color = "yellow"
+main.style.background = "grey"
+
+let button = document.querySelectorAll("button")
+for ( let i=0;i< button.length;i++){
+    button[i].style.color = "yellow"
+    button[i].style.background = "black"
+}
+
+let p = document.querySelectorAll("p")
+for ( let i=0;i< p.length;i++){
+    p[i].style.color = "yellow"
+}
+}
+let jour = true
+function light(){
+    for (let jour = 0; jour<! dark.length; i++) {
+    }
+    if (dark = false) {
+        light = true
+    }
+}
+
